d7e83928fc5134290d905deb35eb6a5a234dcb70
claudiopatti <claudiopatti1997@gmail.com>
2024-07-19 18:47:33 +0200

    esercizo Palindromo fatto iniziato esercizo pari e ho fatto fino alla somma

--- Palindroma/js/scripts.js
+++ Palindroma/js/scripts.js
@@ -1,1 +1,27 @@
-console.log('ciao console')
+
+function checkPalindrome(parola) {
+
+    
+    const arrayValues = parola.split('');
+
+    
+    const reverseArrayValues = arrayValues.reverse();
+
+    
+    const reverseParola = reverseArrayValues.join('');
+
+    if(parola == reverseParola) {
+        alert('E\' palindromo');
+        console.log('E\' palindromo')
+    }
+    else {
+        alert('Non è palindromo');
+        console.log('Non è palindromo')
+    }
+}
+
+
+const parola = prompt('Inserisci una parola: ');
+
+checkPalindrome(parola);
+
--- Pari e dispari/js/scripts.js
+++ Pari e dispari/js/scripts.js
@@ -1,1 +1,48 @@
-console.log('ciao console')
+let inputPari = document.getElementById('selectPari');
+
+inputPari.addEventListener('click', 
+    function () {
+        console.log('pari')
+    }
+);
+
+let inputDispari = document.getElementById('selectDispari');
+
+inputDispari.addEventListener('click', 
+    function () {
+        console.log('dispari')
+    }
+);
+
+
+
+// const numero = parseInt(prompt('inserisci un numero da 1 a 5'));
+// console.log('number',numero);
+
+// const numberRandom = Math.floor(Math.random() * 5 ) + 1;
+// console.log(numberRandom);
+
+// const sum = numberRandom + numero;
+// console.log('sum', sum)
+const numero = parseInt(prompt('inserisci un numero da 1 a 5'));
+console.log('number',numero);
+
+const numberRandom = Math.floor(Math.random() * 5 ) + 1;
+console.log(numberRandom);
+
+winner();
+function winner() {
+
+
+
+    const sum = numberRandom + numero;
+    console.log('sum', sum)
+
+    if (sum % 2 == 0) {
+        console.log('è pari')
+    }
+    else {
+        console.log('è dispari')
+    }
+    
+}
